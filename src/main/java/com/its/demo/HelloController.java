9b86565e2f09e70b06ed749c3af98afe14607a36
corso-its-2026 <f.panico@betacom.it>
2026-06-08 02:23:45 +0200

    Initial commit

---
+++ src/main/java/com/its/demo/HelloController.java
@@ -0,0 +1,15 @@
+package com.its.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/demo")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
