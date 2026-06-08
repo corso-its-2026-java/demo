b717ee99eb218bb27761788e1346fbaee1df470a
corso-its-2026 <f.panico@betacom.it>
2026-06-08 16:58:00 +0200

    create helloController

--- src/main/java/com/its/demo/HelloController.java
+++ src/main/java/com/its/demo/HelloController.java
@@ -1,15 +1,23 @@
 package com.its.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/demo")
+@RequestMapping("api/demo")
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
+    public String hello(){
         return "Hello World";
+    }
+
+    @GetMapping("/{nome}")
+    public String helloUtente(@PathVariable String nome){
+        return "Hello "+nome;
+    }
+
+    @GetMapping("/params")
+    public String helloParams(@RequestParam String nome){
+        return "Hello "+nome;
     }
 }
